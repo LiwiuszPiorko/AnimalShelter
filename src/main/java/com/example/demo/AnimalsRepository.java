d45f7ba36ea79ff625f5ef442c271b705fc23f73
Liwiusz Piórko <liwiuszpiorko@wp.pl>
2018-11-30 11:20:34 +0100

    v2

--- src/main/java/com/example/demo/AnimalsRepository.java
+++ src/main/java/com/example/demo/AnimalsRepository.java
@@ -7,16 +7,33 @@ import java.util.List;
 
 @Repository
 public class AnimalsRepository {
-    List<Animals> animalsList = new ArrayList<>();
+    private List<Animals> animalsList = new ArrayList<>();
 
     public List<Animals> getAnimalsList() {
         return animalsList;
     }
-    public AnimalsRepository(){
-        animalsList.add(new Animals("'Burek' ","Pies"));
-        animalsList.add(new Animals("'Miaukot' ","Kot"));
-        animalsList.add(new Animals("'Sssyczek'","Wąż"));
 
+    public AnimalsRepository() {
+        animalsList.add(new Animals("Burek ", "Pies"));
+        animalsList.add(new Animals("Miaukot ", "Kot"));
+        animalsList.add(new Animals("Sssyczek", "Wąż"));
+    }
 
-}
+    public List<Animals> getAnimals() {
+        return animalsList;
+    }
+
+    public void add(Animals animal) {
+        animalsList.add(animal);
+    }
+
+    public List<Animals> getAnimalSpecies(String gatunek) {
+        List<Animals> filtered=new ArrayList<>();
+        for (Animals animals : animalsList) {
+            if (gatunek.equals(animals.getSpecies()))
+                filtered.add(animals);
+
+        }
+        return filtered;
+    }
 }
--- src/main/java/com/example/demo/ShelterControler.java
+++ src/main/java/com/example/demo/ShelterControler.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class ShelterControler {
     private AnimalsRepository repository;
-    private List<Animals> animals;
 
     @Autowired
     public ShelterControler(AnimalsRepository repository) {
@@ -23,13 +23,26 @@ public class ShelterControler {
         List<Animals> animals = repository.getAnimalsList();
         String response = "";
         model.addAttribute("pupile", animals);
-        model.addAttribute("newTask", new Animals());
+        model.addAttribute("newAnimals", new Animals());
+
         return "animals";
     }
-    @PostMapping("/dodaj")
-    public String add(Animals animal){
-       animals.add(animal);
 
+    @PostMapping("/saveAnimal")
+    public String add(Animals animal) {
+        repository.add(animal);
+        return "redirect:/shelter";
+    }
+    @GetMapping("/sortAnimals")
+    public String sortAnimals(@RequestParam String gatunek, Model model){
+        List<Animals>sortedAnimals=repository.getAnimalSpecies(gatunek);
+        model.addAttribute("pupile", sortedAnimals);
+        model.addAttribute("newAnimals", new Animals());
+        return "animals";
+    }
+    @GetMapping("/showAnimal")
+    public String showAnimalProfile(Model model){
+        return "Info";
     }
 }
 
